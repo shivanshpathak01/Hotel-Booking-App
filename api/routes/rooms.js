7d6812b71428f26fd578387617c1dd15333c5f63
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-12-28 18:35:52 +0530

    rooms route done

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+//Create 
+    router.post('/:hotelid',verifyAdmin, createRoom);
+// Update 
 
+router.put('/:id', verifyAdmin,updateRoom)
+
+// Delete
+
+router.delete('/:id/:hotelid',verifyAdmin, deleteRoom)
+
+// Get
+
+router.get('/:id', getRoom)
+
+// GetAll
+router.get('/', getRooms)
 
 
 
