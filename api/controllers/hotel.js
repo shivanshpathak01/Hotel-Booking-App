e031e868b6476a7a16079ebebbdd4354824d0e13
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-12-28 18:25:52 +0530

    hotelCRUD operations done

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,3 +1,5 @@
+import Hotel from "../models/Hotel.js";
+
 export const createHotel = async (req, res) => {
     const newHotel = new Hotel(req.body);
     try {
@@ -19,29 +21,26 @@ export const updateHotel = async (req, res) => {
 
 
 export const deleteHotel = async (req, res) => {
-    const newHotel = new Hotel(req.body);
     try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (err) {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted...");
+    } catch (error) {
         next(err)
     }
 }
 
 export const getHotel = async (req, res) => {
-    const newHotel = new Hotel(req.body);
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (err) {
+        try {
+            const hotel = await Hotel.findById(req.params.id);
+            res.status(200).json(hotel);
+        } catch (error) {
         next(err)
     }
 }
 export const getHotels = async (req, res) => {
-    const newHotel = new Hotel(req.body);
     try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
     } catch (err) {
         next(err)
     }
