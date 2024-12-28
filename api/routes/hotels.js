4c62056c3a316b3c02cc1a9ca0825860babf9a20
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-12-28 18:35:36 +0530

    hotels route done

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,49 +1,26 @@
 import express from 'express';
 import Hotel from '../models/Hotel.js';
-import { createHotel, updateHotel } from '../controllers/hotel.js';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
-const router = express.Router();
+export const router = express.Router();
 
 //Create 
-    router.post('/', createHotel);
+    router.post('/',verifyAdmin, createHotel);
 // Update 
 
-router.put('/:id', updateHotel)
+router.put('/:id', verifyAdmin,updateHotel)
 
 // Delete
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const updatedHotel = await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("Hotel has been deleted...");
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+router.delete('/:id',verifyAdmin, deleteHotel)
 
 // Get
 
-router.get('/:id', async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+router.get('/:id', getHotel)
 
 // GetAll
-router.get('/', async (req, res,next) => {
-    
-    if(failed) return next(createError(401, 'You are not authorized'));
-
-    try {
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
-    } catch (err) {
-        next(err)
-    }
-})
+router.get('/', getHotels)
 
 
 export default router;
