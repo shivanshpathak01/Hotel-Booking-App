245c106733408920e1d2f17fafcbe09152ff6a0f
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-12-28 18:35:15 +0530

    auth route done

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import { register,login } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Auth endpoint');
-});
+router.post('/register', register);
+router.post('/login', login);
 
 
 
