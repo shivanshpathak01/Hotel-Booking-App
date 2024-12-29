38e1713e964c660a676cc83db75be4e7fd806f24
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-12-29 13:09:39 +0530

    added some operations for count

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -45,4 +45,15 @@ export const getHotels = async (req, res) => {
         next(err)
     }
 }
+export const countByCity = async (req, res) => {
+    const cities = req.query.cities.split(',');
+    try {
+        const list = await Promise.all(cities.map(city => {
+            return Hotel.countDocuments({city: city})    
+        }))
+        res.status(200).json(list);
+    } catch (err) {
+        next(err)
+    }
+}
 
